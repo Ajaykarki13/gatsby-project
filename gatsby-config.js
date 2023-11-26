68020b6853c56a766d57be6c15acffdfd448d9a5
Ajay <ajaymum698@gmail.com>
2023-11-26 15:39:39 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `landing page`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-styled-components"]
+};
